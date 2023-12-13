2e0211d78eccfacd4597a94b367c9c53bbbc8050
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2023-12-13 00:18:47 -0300

    Adiciona exemplo de uso .env

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,11 +3,16 @@
 import Counter from '@/components/counter.vue'
 import Potatoes from '@/components/potatoes.vue'
 
+const envExample = import.meta.env.VITE_ENV_EXAMPLE
+
 </script>
 
 <template>
   <div class="flex flex-col">
     <Counter title="Batatinhas"/>
     <Potatoes title="Batatinhas"/>
+    <p class="text-center">
+      {{ envExample }}
+    </p>
   </div>
 </template>
