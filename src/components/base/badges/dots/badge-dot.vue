2a3f0812f6e2f550dd27945c48c2c922a70e7bbc
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2025-08-24 16:36:07 -0300

    Atualiza dependências do projeto e adiciona componente DotsBar com BadgeDots

---
+++ src/components/base/badges/dots/badge-dot.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+defineProps({
+  variant: {
+    type: String,
+    default: 'primary'
+  }
+})
+
+</script>
+
+<template>
+  <div class="dot-wrapper">
+    <div :class="`dot dot-${variant}`" />
+  </div>
+</template>
+
+<style lang="sass" scoped>
+
+.dot
+  width: 100%
+  height: 100%
+  border-radius: 9999px
+
+.dot-wrapper
+  width: 12px
+  height: 12px
+  border-radius: 9999px
+  padding: 0
+  margin: 0
+
+.dot-primary
+  background-color: var(--primary-color)
+
+.dot-secondary
+  background-color: var(--secondary-color)
+
+.dot-success
+  background-color: var(--success-color)
+
+.dot-warning
+  background-color: var(--warning-color)
+
+.dot-danger
+  background-color: var(--danger-color)
+
+.dot-disabled
+  background-color: var(--disabled-color)
+
+</style>
---
+++ src/components/dots-bar.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+import { IconDots } from '@tabler/icons-vue'
+
+import BadgeDot from '@/components/base/badges/dots'
+import ContainerCard from '@/components/container-card.vue'
+import LeadBlock from '@/components/lead-block.vue'
+
+defineProps({
+  title: {
+    type: String,
+    default: 'Dots Badge'
+  }
+})
+
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <ContainerCard>
+      <LeadBlock :title="title">
+        <IconDots
+          :width="24"
+          :height="24"
+          :stroke="1.5"
+        />
+      </LeadBlock>
+
+      <div class="flex flex-row justify-center flex-wrap gap-4">
+        <BadgeDot variant="primary" />
+        <BadgeDot variant="secondary" />
+        <BadgeDot variant="success" />
+        <BadgeDot variant="warning" />
+        <BadgeDot variant="danger" />
+        <BadgeDot variant="disabled" />
+      </div>
+    </ContainerCard>
+  </div>
+</template>
