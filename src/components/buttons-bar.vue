68cb0b342e73348bfca2ad99e79d33bf2371de9a
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2026-02-14 08:57:02 -0300

    Atualiza dependências e formatação de código

--- src/components/buttons-bar.vue
+++ src/components/buttons-bar.vue
@@ -1,43 +1,43 @@
 <script setup>
-
-import { ref } from 'vue'
-import { useIntervalFn } from '@vueuse/core'
 import { IconHandFinger, IconMoodKidFilled } from '@tabler/icons-vue'
+import { useIntervalFn } from '@vueuse/core'
+import { ref } from 'vue'
 
 import Button from '@/components/base/custom-button'
 import ContainerCard from '@/components/container-card.vue'
 import LeadBlock from '@/components/lead-block.vue'
-import { VARIANTS, BUTTON_COLOR_CHANGE_TIMEOUT } from '@/constants'
+import { BUTTON_COLOR_CHANGE_TIMEOUT, VARIANTS } from '@/constants'
 
 defineProps({
   title: {
     type: String,
-    default: 'Buttons'
+    default: 'Buttons',
   },
   subtitle: {
     type: String,
-    default: 'Buttons with icons'
-  }
+    default: 'Buttons with icons',
+  },
 })
 
 const coloredButtonsVariant = ref(0)
 
-useIntervalFn(() => {
-  const condition = coloredButtonsVariant.value >= VARIANTS.length - 1
-  condition ? coloredButtonsVariant.value = 0 : coloredButtonsVariant.value++
-}, BUTTON_COLOR_CHANGE_TIMEOUT, { immediate: true })
-
+useIntervalFn(
+  () => {
+    const condition = coloredButtonsVariant.value >= VARIANTS.length - 1
+    condition
+      ? (coloredButtonsVariant.value = 0)
+      : coloredButtonsVariant.value++
+  },
+  BUTTON_COLOR_CHANGE_TIMEOUT,
+  { immediate: true },
+)
 </script>
 
 <template>
   <div class="flex flex-col">
     <ContainerCard>
       <LeadBlock :title="title">
-        <IconHandFinger
-          :width="24"
-          :height="24"
-          :stroke="1.5"
-        />
+        <IconHandFinger :width="24" :height="24" :stroke="1.5" />
       </LeadBlock>
 
       <div class="flex flex-row justify-center flex-wrap gap-2">
@@ -58,45 +58,23 @@ useIntervalFn(() => {
 
     <ContainerCard>
       <div class="flex flex-row gap-2">
-        <IconHandFinger
-          width="24"
-          height="24"
-          :stroke="1.5"
-        />
+        <IconHandFinger width="24" height="24" :stroke="1.5" />
         <span>
           {{ subtitle }}
         </span>
       </div>
 
       <div class="flex flex-row justify-center flex-wrap gap-2">
-        <Button
-          label="Icon Prepend"
-          variant="secondary"
-        >
-          <IconMoodKidFilled
-            class="w-6 h-6"
-            :stroke="2"
-          />
+        <Button label="Icon Prepend" variant="secondary">
+          <IconMoodKidFilled class="w-6 h-6" :stroke="2" />
         </Button>
 
-        <Button
-          label="Icon Append"
-          icon-position="append"
-        >
-          <IconMoodKidFilled
-            class="w-6 h-6"
-            :stroke="2"
-          />
+        <Button label="Icon Append" icon-position="append">
+          <IconMoodKidFilled class="w-6 h-6" :stroke="2" />
         </Button>
 
-        <Button
-          icon-position="append"
-          variant="success"
-        >
-          <IconMoodKidFilled
-            class="w-6 h-6"
-            :stroke="2"
-          />
+        <Button icon-position="append" variant="success">
+          <IconMoodKidFilled class="w-6 h-6" :stroke="2" />
         </Button>
       </div>
     </ContainerCard>
